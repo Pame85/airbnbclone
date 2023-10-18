972d6dc65784eb0cc755eb1e1ef2be25dacd6b3d
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-10-18 16:32:13 +0800

    change empty?(strings) to nil?/blank?(integer)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -20,15 +20,15 @@ class BookingsController < ApplicationController
     
     @booking.listing_id = @listing.id
 
-    # if available_dates?(@booking.check_in, @booking.check_out, @listing)
-    #   if @booking.save
-    #     redirect_to booking_path(@booking), notice: "Booking created"
-    #   else
-    #     render :new
-    #   end
-    # else
-    #   redirect_to listings_path(@listing), notice: "Booking failed"
-    # end
+    if available_dates?(@booking.check_in, @booking.check_out, @listing)
+      if @booking.save
+        redirect_to booking_path(@booking), notice: "Booking created"
+      else
+        render :new
+      end
+    else
+      redirect_to listings_path(@listing), notice: "Booking failed"
+    end
   end
 
   private
@@ -36,14 +36,14 @@ class BookingsController < ApplicationController
       params.require(:booking).permit(:listing_id, :check_in, :check_out)
     end
 
-    # def available_dates?(ci_date, co_date, listing)
-    #   if listing.present?
-    #     overlap_bookings = listing.bookings.where(check_in: ci_date..co_date)
-    #       .or(listing.bookings.where(check_out: ci_date..co_date))
-    #     overlap_bookings.empty?
-    #   else
-    #     false
-    #   end
-    # end
+    def available_dates?(ci_date, co_date, listing)
+      if listing.present?
+        overlap_bookings = listing.bookings.where(check_in: ci_date..co_date)
+          .or(listing.bookings.where(check_out: ci_date..co_date))
+        overlap_bookings.blank?
+      else
+        false
+      end
+    end
     
 end
